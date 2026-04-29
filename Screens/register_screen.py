130e5412bbc0d41f966fe01728265e2d7bc9ae16
APatrick8 <aidan.patrick@live.com>
2026-04-29 17:04:00 -0500

    Made backend functions into classes to improve readability and fix import errors.

--- Screens/register_screen.py
+++ Screens/register_screen.py
@@ -1,10 +1,10 @@
 from kivy.uix.screenmanager import Screen
-
+from kivy.app import App
 
 class RegisterScreen(Screen):
     def do_register(self, username, password):
-        app = self.manager.app
-        ok, res = app.backend.get("auth_service").register(username, password)
+        app = App.get_running_app()
+        ok, res = app.auth.register(username, password)
         if ok:
             app.goto("login")
         else:
--- Screens/settings_screen.py
+++ Screens/settings_screen.py
@@ -1,12 +1,13 @@
 from kivy.uix.screenmanager import Screen
+from kivy.app import App
 
 
 class SettingsScreen(Screen):
     def toggle_dark(self, enabled: bool):
-        app = self.manager.app
+        app = App.get_running_app()
         app.apply_theme(dark=enabled, font_size_name=app._font_size_name)
 
     def set_font(self, size_name: str):
-        app = self.manager.app
+        app = App.get_running_app()
         app._font_size_name = size_name
         app.apply_theme(dark=app._dark, font_size_name=size_name)
