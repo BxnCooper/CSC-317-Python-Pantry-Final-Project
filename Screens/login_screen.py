0f1968e65b5902ef580d7579ea46395b6a62a0cd
APatrick8 <aidan.patrick@live.com>
2026-04-29 19:28:35 -0500

    Fixes register screen error and merges Benjamin's volunteer portal functions with object oriented backend

--- Screens/login_screen.py
+++ Screens/login_screen.py
@@ -6,6 +6,7 @@ class LoginScreen(Screen):
         app = App.get_running_app()
         ok, res = app.auth.login(username, password)
         if ok:
+            app.current_user = res
             app.goto("dashboard")
         else:
             print("Login failed:", res)
